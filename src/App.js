b1ea03dab10673d8071a04fbcd5a5f481d198389
Ramires <moreira.ramires@gmail.com>
2022-07-09 20:06:41 -0300

    started api conect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      Hello World
+      {/* Navbar */}
+      {/* Destaques */}
+      {/* Em alta */}
+      {/* Filmes por categoria */}
     </div>
   );
 }
